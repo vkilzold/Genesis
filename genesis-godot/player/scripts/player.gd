0e34eed2af1b931baadd146586aa8e7f3dab8105
prince <princemaynardmasilac@gmail.com>
2025-01-14 15:53:04 +0800

    new testing. added proper scene and set up for idle animation

---
+++ genesis-godot/player/scripts/player.gd
@@ -0,0 +1,22 @@
+class_name Player extends CharacterBody2D
+
+var move_speed : float = 200.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction : Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	velocity = direction * move_speed
+	
+	
+	
+	pass
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
